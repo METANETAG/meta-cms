3cf69e9b294eafb7f96db0ffe4296d7445c21ed1
TiMESPLiNTER <github@timesplinter.ch>
2015-04-16 10:16:58 +0200

    Improved FileHandler::storeFile

--- src/ch/metanet/cms/model/Model.php
+++ src/ch/metanet/cms/model/Model.php
@@ -8,22 +8,12 @@ use timesplinter\tsfw\db\DB;
  * @author Pascal Muenst <entwicklung@metanet.ch>
  * @copyright Copyright (c) 2013 by METANET AG, www.metanet.ch
  */
-class Model
+abstract class Model
 {
 	protected $db;
 
 	public function __construct(DB $db)
 	{
 		$this->db = $db;
-	}
-
-	protected static function setLockedProperty($obj, $property, $value)
-	{
-		$refClass = new \ReflectionClass($obj);
-		$propertyID = $refClass->getProperty($property);
-
-		$propertyID->setAccessible(true);
-		$propertyID->setValue($obj, $value);
-		$propertyID->setAccessible(false);
 	}
 }
